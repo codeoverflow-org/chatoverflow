85f3e1eeb554249dee867167b5a824115d258705
sebinside <hi@sebinside.de>
2019-03-13 22:47:52 +0100

    Finished dependency sbt code. Next up: Bootstrap launcher logic.

--- bootstrap/src/main/scala/Bootstrap.scala
+++ bootstrap/src/main/scala/Bootstrap.scala
@@ -8,10 +8,9 @@ object Bootstrap {
   TODO:
   1. Write SBT-Logic to extract all libs from "sbt dependencyList" DONE
   2. Write SBT-logic to transform and save this information as URL List DONE
-  2a. Make the logik copy the built jars into an bin folder inside the resources of the bootstrap
   3. Make the boostrap launcher download everything, create folder structures
      and launch chat overflow with custom classpath
-  4. Make fat jar with only the scala lib inside (and the chat overflow files?)
+  4. Make fat jar with only the scala lib inside
    */
 
 }
