17c5f13c58417e5c02ad8647058bf7d04072cbb8
sebinside <hi@sebinside.de>
2019-04-06 22:35:44 +0200

    Added the idea of UI utilities.

---
+++ src/main/scala/org/codeoverflow/chatoverflow/ui/common/UIUtilities.scala
@@ -0,0 +1,13 @@
+package org.codeoverflow.chatoverflow.ui.common
+
+import org.codeoverflow.chatoverflow.ChatOverflow
+
+/**
+  * UIUtilities encapsulate common requests to the chat overflow framework that
+  * are used by more than one UIs, e.g. REPL and GUI.
+  *
+  * @param chatOverflow the chat overflow framework object to operate on
+  */
+class UIUtilities(chatOverflow: ChatOverflow) {
+  // TODO: Refactor REPL & WIP-GUI. Most of the work should be brought together, here.
+}
