2732c7b794a515228bbd58514b7d91617ed38fc2
Tobias Manske <tobias.manske@mailbox.org>
2019-04-20 01:06:41 +0200

    Simplify pathSeperator determination
    Signed-off-by: Tobias Manske <tobias.manske@mailbox.org>

--- bootstrap/src/main/scala/Bootstrap.scala
+++ bootstrap/src/main/scala/Bootstrap.scala
@@ -29,12 +29,6 @@ object Bootstrap {
     if (testValidity()) {
       println("Valid ChatOverflow installation. Checking libraries...")
 
-      var OSSeparator = ":"
-      if (System.getProperty("os.name").toLowerCase.contains("windows")) {
-        println("You're running windows. Changing path separator.")
-        OSSeparator = ";"
-      }
-
       if (checkLibraries(args)) {
         val javaPath = createJavaPath()
         if (javaPath.isDefined) {
@@ -47,7 +41,7 @@ object Bootstrap {
           // TODO: Fix chat overflow config service to handle non existent config folder. I mean... what?
 
           // Start chat overflow!
-          val process = new java.lang.ProcessBuilder(javaPath.get, "-cp", s"bin/*${OSSeparator}lib/*", chatOverflowMainClass)
+          val process = new java.lang.ProcessBuilder(javaPath.get, "-cp", s"bin/*${File.pathSeparator}lib/*", chatOverflowMainClass)
             .inheritIO().start()
 
           val exitCode = process.waitFor()
