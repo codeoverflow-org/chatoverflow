2e31be6b50f016710fff33ba3d2f3a13e2f84851
Tobias Manske <tobias.manske@mailbox.org>
2019-05-11 23:58:14 +0200

    [WIP] TwitterConnector - Can send Tweet now
    Signed-off-by: Tobias Manske <tobias.manske@mailbox.org>

--- src/main/scala/org/codeoverflow/chatoverflow/requirement/service/twitter/TwitterConnector.scala
+++ src/main/scala/org/codeoverflow/chatoverflow/requirement/service/twitter/TwitterConnector.scala
@@ -1,12 +1,17 @@
 package org.codeoverflow.chatoverflow.requirement.service.twitter
 
-import org.codeoverflow.chatoverflow.WithLogger
-import org.codeoverflow.chatoverflow.connector.Connector
 import com.danielasfregola.twitter4s.TwitterRestClient
 import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken}
+import org.codeoverflow.chatoverflow.WithLogger
+import org.codeoverflow.chatoverflow.connector.Connector
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 /**
   * The twitch connector connects to the irc service to work with chat messages.
   * todo fix me
+  *
   * @param sourceIdentifier the name to the twitch account
   */
 class TwitterConnector(override val sourceIdentifier: String) extends Connector(sourceIdentifier) with WithLogger {
@@ -23,15 +28,26 @@ class TwitterConnector(override val sourceIdentifier: String) extends Connector(
 
   override def start(): Boolean = {
 
-      val consumerToken = credentials.get.getValue(consumerTokenConfig)
-      val accessToken = credentials.get.getValue(accessTokenConfig)
+    val consumerToken = credentials.get.getValue(consumerTokenConfig).get
+    val accessToken = credentials.get.getValue(accessTokenConfig).get
 
-      val consumerSecret = credentials.get.getValue(consumerSecretConfig)
-      val accessSecret = credentials.get.getValue(accessSecretConfig)
+    val consumerSecret = credentials.get.getValue(consumerSecretConfig).get
+    val accessSecret = credentials.get.getValue(accessSecretConfig).get
 
-      //fixme: orNull may crash the Connector
-      client = new TwitterRestClient(ConsumerToken(consumerToken.orNull, consumerSecret.orNull), AccessToken(accessToken.orNull, accessSecret.orNull))
-      true
+    client = new TwitterRestClient(ConsumerToken(consumerToken, consumerSecret), AccessToken(accessToken, accessSecret))
+    true
+  }
+
+  def getTimeline: String = {
+    Await.result(client.homeTimeline(count = 1), 5 seconds).data.headOption match {
+      case Some(t) => t.text
+      case None => "None"
+    }
+  }
+
+  def sendTweet(status: String) = {
+    Await.result(client.createTweet(status), 5 seconds)
+    true
   }
 
 
@@ -41,5 +57,7 @@ class TwitterConnector(override val sourceIdentifier: String) extends Connector(
   /**
     * This stops the activity of the connector, e.g. by closing the platform connection.
     */
-  override def stop(): Boolean = { true }
+  override def stop(): Boolean = {
+    true
+  }
 }
