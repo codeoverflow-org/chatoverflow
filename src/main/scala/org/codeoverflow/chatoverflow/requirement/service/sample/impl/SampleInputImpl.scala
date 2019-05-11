cc8835d6d57c30c895bcebb95a3f60dc92ec4db0
sebinside <hi@sebinside.de>
2019-05-11 20:52:26 +0200

    Finished sample connector.

--- src/main/scala/org/codeoverflow/chatoverflow/requirement/service/sample/impl/SampleInputImpl.scala
+++ src/main/scala/org/codeoverflow/chatoverflow/requirement/service/sample/impl/SampleInputImpl.scala
@@ -2,9 +2,11 @@ package org.codeoverflow.chatoverflow.requirement.service.sample.impl
 
 import org.codeoverflow.chatoverflow.WithLogger
 import org.codeoverflow.chatoverflow.api.io.input.SampleInput
+import org.codeoverflow.chatoverflow.registry.Impl
 import org.codeoverflow.chatoverflow.requirement.Connection
 import org.codeoverflow.chatoverflow.requirement.service.sample.SampleConnector
 
+@Impl(impl = classOf[SampleInput], connector = classOf[SampleConnector])
 class SampleInputImpl extends Connection[SampleConnector] with SampleInput with WithLogger {
   override def init(): Boolean = sourceConnector.get.init()
 
