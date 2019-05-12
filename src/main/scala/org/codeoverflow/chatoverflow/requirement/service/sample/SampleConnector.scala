37994db7db15078b6fe9e94baa59e0622c194df4
Jonas <joblo.2213@gmail.com>
2019-05-12 09:56:29 +0200

    Further improved documentation

--- src/main/scala/org/codeoverflow/chatoverflow/requirement/service/sample/SampleConnector.scala
+++ src/main/scala/org/codeoverflow/chatoverflow/requirement/service/sample/SampleConnector.scala
@@ -5,12 +5,14 @@ import org.codeoverflow.chatoverflow.connector.Connector
 
 class SampleConnector(override val sourceIdentifier: String) extends Connector(sourceIdentifier) with WithLogger {
   override protected var requiredCredentialKeys: List[String] = List()
-  override protected var optionalCredentialKeys: List[String] = List()
+  override protected var optionalCredentialKeys: List[String] = List("optionalSample")
 
   /**
     * Starts the connector, e.g. creates a connection with its platform.
     */
   override def start(): Boolean = {
+    val optionalSample: Option[String] = credentials.get.getValue("optionalSample")
+    if (optionalSample.isEmpty) logger info "Credentials value optionalSample was not set"
     logger info s"Started sample connector! Source identifier is: '$sourceIdentifier'."
     true
   }
