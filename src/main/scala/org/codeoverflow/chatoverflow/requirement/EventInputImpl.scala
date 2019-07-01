7a9312e27f8da92bde3405f70733901d9498ea36
Jonas <joblo.2213@gmail.com>
2019-07-01 19:30:30 +0200

    Fix type mismatch by using TypeTags and cast

--- src/main/scala/org/codeoverflow/chatoverflow/requirement/EventInputImpl.scala
+++ src/main/scala/org/codeoverflow/chatoverflow/requirement/EventInputImpl.scala
@@ -6,23 +6,26 @@ import org.codeoverflow.chatoverflow.api.io.event.Event
 import org.codeoverflow.chatoverflow.requirement.EventInputImpl.EventHandler
 
 import scala.collection.mutable.ListBuffer
+import scala.reflect.runtime.universe
 
 abstract class EventInputImpl {
 
   private val handlers = ListBuffer[EventHandler[_]]()
 
-  def registerEventHandler[T <: Event](eventHandler: Consumer[T]): Unit = {
-    handlers += new EventHandler[T](eventHandler)
+  def registerEventHandler[T <: Event](implicit tt: universe.TypeTag[T],  eventHandler: Consumer[T]): Unit = {
+    handlers += new EventHandler[T](tt, eventHandler)
   }
 
-  def call[T <: Event](event: T): Unit = {
+  def call[T <: Event](implicit tt: universe.TypeTag[T], event: T): Unit = {
     handlers.filter(h => {
       h.clazz == classOf[T]
-    }).foreach(h => h.consumer.accept(event)) // FIXME Type mismatch
+    }).foreach(h => h.consumer.accept(h.cast(event))) // TODO Test if working
   }
 }
 object EventInputImpl {
-  private class EventHandler[T >: Event](val consumer: Consumer[T]) {
+  private class EventHandler[T <: Event](private val tt: universe.TypeTag[T], val consumer: Consumer[T]) {
     val clazz: Class[T] = classOf[T]
+
+    def cast[A](a: Any): A = a.asInstanceOf[A]
   }
 }
