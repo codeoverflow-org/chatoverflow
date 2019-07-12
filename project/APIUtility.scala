c426efaf0eb3a7eaeb408aad0e43dbc51a085896
sebinside <hi@sebinside.de>
2019-07-12 22:41:00 +0200

    Changed approach. Reflection would cause circular dependencies. So, text file operations, here we go again.

--- project/APIUtility.scala
+++ project/APIUtility.scala
@@ -1,9 +1,13 @@
 import java.io.File
+
 import sbt.internal.util.ManagedLogger
 import sbt.io.IO
 
+import scala.collection.mutable.ListBuffer
+
 /**
   * The API utility encapsulates methods for building the chatoverflow API.
+  *
   * @param logger The current logger instance. Usually: {{{streams.value.log}}}
   */
 class APIUtility(logger: ManagedLogger) {
@@ -11,23 +15,43 @@ class APIUtility(logger: ManagedLogger) {
   /**
     * Generates the requirement classes input, output and parameter from annotated types in the api.
     */
-  def generatedRequirements(): Unit = {
+  def generatedRequirements(sourceDirectory: File): Unit = {
     println("I'm not implemented yet.")
 
-    // Not working
-    /*
-    val reflections: Reflections = new Reflections(new ConfigurationBuilder()
-      .setUrls(ClasspathHelper.forPackage(...))
-      .setScanners(new SubTypesScanner(), new TypeAnnotationsScanner()))
-    val classes = reflections.getTypesAnnotatedWith(classOf[IsRequirement])
-    */
+    // Short explanation: Using reflection would cause circular dependencies between classes and build environment.
+    // So, looking up the source files is considered a dirty, but cleaner solution
+
+    val ioDirectory = new File(sourceDirectory, "main/java/org/codeoverflow/chatoverflow/api/io")
+    val subDiretories = Seq(new File(ioDirectory, "input"),
+      new File(ioDirectory, "output"), new File(ioDirectory, "parameter"))
+
+    var sourceFiles = ListBuffer[File]()
+
+    sourceFiles ++= subDiretories.flatMap(file => file.listFiles().toList)
+
+    // Recursively add child files
+    for (i <- 1 to 5) {
+      sourceFiles = addChilds(sourceFiles)
+    }
+
+    // TODO: Now, every file should be parsed for the Annotation and the interface name using regex
+  }
+
+  private def addChilds(sourceFiles: ListBuffer[File]): ListBuffer[File] = {
+    for (sourceFile <- sourceFiles) {
+      if (sourceFile.isDirectory) {
+        sourceFiles ++= sourceFile.listFiles().filter(file => !sourceFiles.contains(file))
+      }
+    }
+    sourceFiles
   }
 
   /**
     * Generates the API version file based on the values in the build file.
+    *
     * @param sourceDirectory the sourceDirectory from sbt {{{sourceDirectory.value}}}
-    * @param majorVersion the major api version
-    * @param minorVersion the minor api version
+    * @param majorVersion    the major api version
+    * @param minorVersion    the minor api version
     */
   def generateAPIVersionFile(sourceDirectory: File, majorVersion: Int, minorVersion: Int): Unit = {
     val file = new File(sourceDirectory, "main/java/org/codeoverflow/chatoverflow/api/APIVersion.java")
