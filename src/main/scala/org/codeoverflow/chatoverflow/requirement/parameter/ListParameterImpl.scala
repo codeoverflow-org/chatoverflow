b93b8592a28614e9e9e57a375f37227fe03da8a2
derNiklaas <derNiklaas@users.noreply.github.com>
2019-11-09 00:12:48 +0100

    Implemented changes from @justgerd

--- src/main/scala/org/codeoverflow/chatoverflow/requirement/parameter/ListParameterImpl.scala
+++ src/main/scala/org/codeoverflow/chatoverflow/requirement/parameter/ListParameterImpl.scala
@@ -1,13 +1,11 @@
 package org.codeoverflow.chatoverflow.requirement.parameter
 
-import java.util.Collections
 import java.util.List
 
 import collection.JavaConverters._
 import org.codeoverflow.chatoverflow.api.io.parameter.ListParameter
 import org.codeoverflow.chatoverflow.registry.Impl
 
-import scala.collection.mutable.ListBuffer
 
 /**
   * A parameter holding a List<String> value.
@@ -19,30 +17,13 @@ class ListParameterImpl extends ListParameter {
   override def getType: Class[List[String]] = classOf[List[String]]
 
   override def serialize(): String = {
-    var out = ""
-    for (part <- value.toArray()) {
-      if (out == "") {
-        out = out + part
-      } else {
-        out = out + "," + part
-      }
-    }
-    out
+    value.asScala.mkString(",")
   }
 
   override def get(): List[String] = value
 
   override def deserialize(value: String): Unit = {
-    var splits = value.split(",");
-    if (splits.length == 1) {
-      set(Collections.singletonList[String](splits(0)))
-    } else {
-      var list: ListBuffer[String] = ListBuffer[String]()
-      for (part <- splits) {
-        list += part
-      }
-      set(mutableSeqAsJavaList[String](list))
-    }
+    set(value.split(",").toSeq.asJava)
   }
 
   override def set(value: List[String]): Unit = this.value = value
