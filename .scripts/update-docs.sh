81818204e260286d290d341b22e5728b2eb5b3cd
Daniel <daniel.hub@outlook.de>
2019-11-24 19:47:57 +0100

    Move java/scaladoc deploying from Travis to GitHub Actions

--- .scripts/update-docs.sh
+++ .scripts/update-docs.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 set -ev
-SCALA_MAJOR=$(echo $TRAVIS_SCALA_VERSION | egrep '^[0-9]+\.[0-9]+' -o)
 
 # Update the java-/scaladocs on https://github.com/codeoverflow-org/chatoverflow-gh-pages
 
-echo "Configuring git..."
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
 echo "Cloning docs repo..."
-git clone https://${GH_TOKEN}@github.com/codeoverflow-org/chatoverflow-gh-pages --branch master --single-branch --depth 1 docs
+git clone https://${REPO_TOKEN}@github.com/codeoverflow-org/chatoverflow-gh-pages --branch master --single-branch --depth 1 docs
 
 echo "Copying docs..."
-cp -fR target/scala-$SCALA_MAJOR/api docs/chatoverflow
-cp -fR api/target/scala-$SCALA_MAJOR/api docs/chatoverflow-api
+cp -fR target/scala-*/api docs/chatoverflow
+cp -fR api/target/scala-*/api docs/chatoverflow-api
 cd docs
 
 echo "Updating wiki..."
 git add .
-git commit --message "Deployed doc (Travis build #$TRAVIS_BUILD_NUMBER)"
+git commit --message "Deployed doc (Action build $GITHUB_ACTION)"
 git push --force origin master
